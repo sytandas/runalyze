3b22e320c9f0d9d2270b421d989efe6a8eef20dd
sytandas <sytandas@gmail.com>
2025-06-12 18:13:14 +0530

    general

--- ana.py
+++ ana.py
@@ -114,6 +114,7 @@ def file_extract(x):
     }
 
 data_1 = file_extract(tree_1)
+print("")
 data_2= file_extract(tree_2)
 
 # Implementation of dynamic time warping (DTW) to compare two runs changing fintess measures.  
@@ -167,7 +168,9 @@ def plot_dtw_alignment(s1_og, s2_og, label='Metric'):
     plt.show()
 
 # plot for pace, hr, cadence etc  
-plot_dtw_alignment(data_1['cadence'], data_2['cadence'], label="cadence")   
+plot_dtw_alignment(data_1['cadence'], data_2['cadence'], label="cadence")
+plot_dtw_alignment(data_1['heart_rate'], data_2['heart_rate'], label="heart_rate")   
+plot_dtw_alignment(data_1['pace'], data_2['pace'], label="pace")
 
 # TODO: Analyzing the ploting how the fitness improved e.g. low hr at same pace, high cadence ~ efficiency.
 # TODO: Visualize trends and optionally apply DTW for route-based comparison 
