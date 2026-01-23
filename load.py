5cef5161b4c45ea94959e10f1c5dce7e431c7d5d
sytandas <sytandas@gmail.com>
2026-01-23 22:46:29 +0530

    todo

---
+++ load.py
@@ -0,0 +1,7 @@
+"""
+Central Idea: 
+Load balance using HR each week.
+Primarily Idea came from Banister Impulse-Response Model.
+Training Load -> Fitness - Fatigue = Performance
+How close become the breakthrough line graphically. 
+"""
