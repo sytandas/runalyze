6f461c91bb7e969418c4e89d1a26071d2822e78b
sytandas <sytandas@gmail.com>
2026-01-25 04:50:53 +0530

    template

--- load.py
+++ load.py
@@ -5,3 +5,24 @@ Primarily Idea came from Banister Impulse-Response Model.
 Training Load -> Fitness - Fatigue = Performance
 How close become the breakthrough line graphically. 
 """
+
+import math, sys
+from datetime import datetime
+import xml.etree.ElementTree as ET
+from statistics import mean, variance
+import numpy as np
+import matplotlib.pyplot as plt
+
+if len(sys.argv) < 3:
+    print("Usage: python3 ana.py <file1.tcx> <file2.tcx>")
+    sys.exit(1)
+
+tree_1 = ET.parse(sys.argv[1]).getroot()
+tree_2 = ET.parse(sys.argv[2]).getroot()
+
+
+# add ActivityExtension namespace
+ns = {
+    'tcx': 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2',
+    'ae':  'http://www.garmin.com/xmlschemas/ActivityExtension/v2'
+}
